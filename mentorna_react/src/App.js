ca4688fc70b8fb2d3737977397329bacf0798331
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-05-21 00:01:37 +0300

    adding redux to the project and routes for the pages

---
+++ mentorna_react/src/App.js
@@ -0,0 +1,52 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css';
+import './fonts/fonts.css'
+import { Header, Marketing, Features, Community, Niche, Testimonials,
+  CaseStudies, Footer,  } from './containers';
+import { Container, createTheme, ThemeProvider, CssBaseline } from '@mui/material';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  // To change the default font-family for whole app
+  const theme = createTheme ({
+    typography: {
+      h3: { 
+        fontFamily:'RecoletaBold',
+      }
+    },
+    Container: {
+      maxWidth: 'xl',
+    },
+  });
+  return (
+    <>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Container>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/dashboard/:id" element={<Dashboard />} />
+            </Routes>
+            <Marketing />
+            <Features />
+            <Community />
+            <Niche />
+            <Testimonials />
+            <CaseStudies />
+            <Footer />
+          </Container>
+        </ThemeProvider>
+      </Router>
+    </>
+  );
+}
+
+export default App;
